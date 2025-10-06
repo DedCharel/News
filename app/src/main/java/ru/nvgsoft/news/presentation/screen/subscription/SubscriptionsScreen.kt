42b3d2d91bff976179f73f60ffafa4c091bf3b09
Daniil Shcherbakov <Danvikmail@gmail.com>
2025-10-06 20:47:04 +0300

    Read and Share article (use Intent)

--- app/src/main/java/ru/nvgsoft/news/presentation/screen/subscription/SubscriptionsScreen.kt
+++ app/src/main/java/ru/nvgsoft/news/presentation/screen/subscription/SubscriptionsScreen.kt
@@ -1,5 +1,6 @@
 package ru.nvgsoft.news.presentation.screen.subscription
 
+import android.content.Intent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -41,14 +42,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import coil3.compose.AsyncImage
+import dagger.hilt.android.qualifiers.ApplicationContext
 import ru.nvgsoft.news.R
 import ru.nvgsoft.news.domain.entity.Article
 import ru.nvgsoft.news.presentation.ui.theme.CustomIcons
@@ -371,9 +375,13 @@ fun ArticleCard(
                 .padding(horizontal = 16.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
+            val context = LocalContext.current
             Button(
                 modifier = Modifier.weight(1f),
-                onClick = {}
+                onClick = {
+                    val intent = Intent(Intent.ACTION_VIEW, article.url.toUri())
+                    context.startActivity(intent)
+                }
             ) {
                 Icon(
                     imageVector = CustomIcons.OpenInNew,
@@ -385,7 +393,13 @@ fun ArticleCard(
 
             Button(
                 modifier = Modifier.weight(1f),
-                onClick = {}
+                onClick = {
+                    val intent = Intent(Intent.ACTION_SEND).apply {
+                        type = "text/plain"
+                        putExtra(Intent.EXTRA_TEXT, "${article.title}\n\n${article.url}")
+                    }
+                    context.startActivity(intent)
+                }
             ) {
                 Icon(
                     imageVector = Icons.Default.Share,
