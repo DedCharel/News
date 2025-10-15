0a5f9812514463339e35e2cffa0e6c120a0426ed
Daniil Shcherbakov <Danvikmail@gmail.com>
2025-10-15 10:15:13 +0300

    Hilt with Workers

--- app/src/main/java/ru/nvgsoft/news/data/background/RefreshDataWorker.kt
+++ app/src/main/java/ru/nvgsoft/news/data/background/RefreshDataWorker.kt
@@ -2,13 +2,17 @@ package ru.nvgsoft.news.data.background
 
 import android.content.Context
 import android.util.Log
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import ru.nvgsoft.news.domain.usecase.UpdateSubscribedArticlesUseCase
 
-class RefreshDataWorker(
-    context: Context,
-    workerParameters: WorkerParameters,
+@HiltWorker
+class RefreshDataWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParameters: WorkerParameters,
     private val updateSubscribedArticlesUseCase: UpdateSubscribedArticlesUseCase
 ): CoroutineWorker(context, workerParameters) {
 
